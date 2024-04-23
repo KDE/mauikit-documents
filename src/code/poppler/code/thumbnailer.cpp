d37a53644349cb0026f6366f8ad9deafb15e5d34
camilo neon <milo.h@aol.com>
2024-04-23 08:45:02 -0500

    fixes tot he thumbnailer

--- src/code/poppler/code/thumbnailer.cpp
+++ src/code/poppler/code/thumbnailer.cpp
@@ -10,32 +10,35 @@
 #include <QUrl>
 
 Thumbnailer::Thumbnailer() : QQuickImageProvider(QQuickImageProvider::Image, QQuickImageProvider::ForceAsynchronousImageLoading)
-
 {
 
 }
 
 QImage Thumbnailer::requestImage(const QString &id, QSize *size, const QSize &requestedSize)
 {
-    QScopedPointer<Poppler::Document> document;
     QImage result;
 
-    document.reset( Poppler::Document::load(QUrl::fromUserInput(id).toLocalFile()).get());
+    auto document = Poppler::Document::load(QUrl::fromUserInput(id).toLocalFile());
 
-    if(!document || document->isLocked())
+    if(!document)
     {
         return result;
     }
+
+    if(document->isLocked())
+    {
+        result = QImage("qrc://img_assets/assets/emblem-locked.svg");
+        return result;
+    }
+
+    document->setRenderHint(Poppler::Document::Antialiasing, true);
+    document->setRenderHint(Poppler::Document::TextAntialiasing, true);
 
     // If the requestedSize.width is 0, avoid Poppler rendering
     // FIXME: Actually it works correctly, but an error is anyway shown in the application output.
     if (requestedSize.width() > 0)
     {
-        document->setRenderHint(Poppler::Document::Antialiasing);
-        document->setRenderHint(Poppler::Document::TextAntialiasing);
-
-        QScopedPointer<Poppler::Page> page;
-        page.reset(document->page(0).get());
+        auto page = document->page(0);
 
         if(!page)
         {
